0f558979cf9aa328a76d37262a7ebd42a2cc07e1
unknown <john0payment@gmail.com>
2013-03-27 09:59:01 -0700

    Added TCPClient and a testing folder. TCP Server will be coming later today.

---
+++ src/CommAudio/Client/TCPClient.cpp
@@ -0,0 +1,162 @@
+#include "TCPClient.h"
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: TCPClient
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: TCPClient(sockaddr_in server)
+--
+-- RETURNS: N/A
+--
+-- NOTES: Constructor for TCPClient. Takes a sockaddr_in.
+----------------------------------------------------------------------------------------------------------------------*/
+TCPClient::TCPClient(sockaddr_in server)
+:_server(server)
+{
+
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: StartClient
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void StartClient()
+--
+-- RETURNS: void
+--
+-- NOTES: Sets up everything the client needs including setting up the socket and connecting to the server.
+----------------------------------------------------------------------------------------------------------------------*/
+void 
+TCPClient::StartClient()
+{
+	int n, ns, bytes_to_read;
+	int err;
+	char *bp; 
+	char rbuf[BUFFER_SIZE], sbuf[BUFFER_SIZE], **pptr;
+	WSADATA WSAData;
+	WORD wVersionRequested;
+
+	wVersionRequested = MAKEWORD(2, 2);
+	err = WSAStartup(wVersionRequested, &WSAData);
+
+	//No usable DLL
+	if(err != 0)
+	{
+		printf ("DLL not found!\n");
+		exit(1);
+	}
+
+	// Create the socket
+	if((_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("Cannot create socket");
+		exit(1);
+	}
+
+	// Connecting to the server
+	if(connect(_socket, (struct sockaddr *)&_server, sizeof(_server)) == -1)
+	{
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		exit(1);
+	}
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: readFromSocket
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void readFromSocket(char* buffer)
+--                 char* buffer - A pointer to a character buffer. It must be at least as large as BUFFER_SIZE
+--
+-- RETURNS: void
+--
+-- NOTES: Reads from the socket to buffer.
+----------------------------------------------------------------------------------------------------------------------*/
+void 
+TCPClient::readFromSocket(char* buffer)
+{
+	int n = 0;
+	int bufferCapacity = BUFFER_SIZE;
+
+	// client makes repeated calls to recv until no more data is expected to arrive.
+	while((n = recv(_socket, buffer, bufferCapacity, 0)) < BUFFER_SIZE)
+	{
+		DWORD temp = GetLastError();
+		buffer += n;
+		bufferCapacity -= n;
+		if(n == 0)
+		{
+			break;
+		}
+	}
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: writeToSocket
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void writeToSocket(char* buffer)
+--                 char* buffer - A pointer to a character buffer. It must be at least as large as BUFFER_SIZE
+--
+-- RETURNS: returns the error message from the4 send call.
+--
+-- NOTES: Writes to the socket from buffer.
+----------------------------------------------------------------------------------------------------------------------*/
+int 
+TCPClient::writeToSocket(char* buffer)
+{
+	return send(_socket, buffer, BUFFER_SIZE, 0);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: closeSocket
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void closeSocket()
+--
+-- RETURNS: void
+--
+-- NOTES: Closes the socket and cleans up the WSA.
+----------------------------------------------------------------------------------------------------------------------*/
+void
+TCPClient::closeSocket()
+{
+	closesocket(_socket);
+	WSACleanup();
+}
---
+++ src/CommAudio/Client/TCPClient.h
@@ -0,0 +1,49 @@
+#ifndef TCP_CLIENT
+#define TCP_CLIENT
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- CLASS: TCPClient
+--
+-- FUNCTIONS: TCPClient(sockaddr_in server)
+--            void StartClient()
+--            void readFromSocket(char* buffer)
+--            int writeToSocket(char* buffer)
+--            void closeSocket()
+--
+-- DATE: 2013/03/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- NOTES: This is a TCP client for sending and recieving data.
+-- 
+----------------------------------------------------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <winsock2.h>
+#include <errno.h>
+
+#define SERVER_TCP_PORT 5150
+#define BUFFER_SIZE     8192
+
+class TCPClient
+{
+public:
+	TCPClient(sockaddr_in server);
+
+	void StartClient();
+
+	void readFromSocket(char* buffer);
+	int writeToSocket(char* buffer);
+
+	void closeSocket();
+private:
+
+	sockaddr_in _server;
+	SOCKET _socket;
+};
+
+#endif
